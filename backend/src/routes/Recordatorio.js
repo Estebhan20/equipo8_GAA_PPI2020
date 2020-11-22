c3052cbf4d248b79ba1ac605039d536a66201964
Estebhan20 <teban.casville@gmail.com>
2020-11-22 17:48:47 -0500

    Modifique Recordatorio con los códigos.

--- backend/src/routes/Recordatorio.js
+++ backend/src/routes/Recordatorio.js
@@ -8,8 +8,8 @@ router.get('/',(req,res)=>{
   res.send('Si funciona')
 })
 
-router.get('/Reciclaje',(req,res)=>{
-  mysqlConnection.query('SELECT * FROM Reciclaje',
+router.get('/Recordatorio',(req,res)=>{
+  mysqlConnection.query('SELECT * FROM Recordatorio',
   (err,rows,fields)=>{
     if(!err)
    {
