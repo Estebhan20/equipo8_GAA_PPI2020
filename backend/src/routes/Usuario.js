815566259c41a748d5c1fa8595dfb2297a794906
Estebhan20 <teban.casville@gmail.com>
2020-11-22 17:36:16 -0500

    Modifique Usuario con los Códigos.

--- backend/src/routes/Usuario.js
+++ backend/src/routes/Usuario.js
@@ -31,17 +31,17 @@ router.post('/Usuario', (req, res) => {
    if(err){
      return console.error(err.message);
    }
-   res.json({message:`Usuario Almacenada en la base de datos`})
+   res.json({message:`Usuario Almacenado en la base de datos`})
  });
 });
 
 router.put('/Usuario/:id', (req,res) => {
-  const {Lugar_de_encuentro,Fecha_y_hora,Descripcion,ID_Usuario} = req.body
+  const {Nombres,Apellidos,ID_Usuario,Celular,telefono,Correo,Contraseña,Ciudad,Calle,Numero,Barrio} = req.body
   const { id } = req.params 
 
-mysqlConnection.query(`UPDATE Reciclaje SET Lugar_de_encuentro = ?,Fecha_y_hora = ?,Descripcion= ?,ID_Usuario= ? WHERE id = ?`,[Lugar_de_encuentro,Fecha_y_hora,Descripcion,ID_Usuario,id], (err, rows,fields) => {
+mysqlConnection.query(`UPDATE Usuario SET Nombres =?,Apellidos =?,ID_Usuario =?,Celular = ?,telefono = ?,Correo = ?,Contraseña = ?,Ciudad = ?,Calle = ?,Numero = ?,Barrio= ? WHERE id = ?`,[Nombres,Apellidos,ID_Usuario,Celular,telefono,Correo,Contraseña,Ciudad,Calle,Numero,Barrio,id], (err, rows,fields) => {
    if(!err){
-    res.json({status: `El Reciclaje ha sido actualizado con éxito`});
+    res.json({status: `El Usuario ha sido actualizado con éxito`});
    }else{
      console.log(err);
    }
