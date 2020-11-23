331b0ad18e7e949cd37d8113749d889aae97b3ea
Estebhan20 <teban.casville@gmail.com>
2020-11-22 21:37:52 -0500

    Cambie gran parte de codigo en Articulo.

--- backend/src/routes/Articulo.js
+++ backend/src/routes/Articulo.js
@@ -27,21 +27,21 @@ router.post('/Articulo', (req, res) => {
   let Articulo = [id,Nombre,Descripcion,Row_3];
   let nuevoArticulo = `INSERT INTO Articulo VALUES (?,?,?,?);`
 
- mysqlConnection.query(nuevoReciclaje,Reciclaje, (err,results,fields) => {
+ mysqlConnection.query(nuevoArticulo,Articulo, (err,results,fields) => {
    if(err){
      return console.error(err.message);
    }
-   res.json({message:`Reciclaje Almacenada en la base de datos`})
+   res.json({message:`Articulo Almacenado en la base de datos`})
  });
 });
 
-router.put('/Reciclaje/:id', (req,res) => {
-  const {Lugar_de_encuentro,Fecha_y_hora,Descripcion,ID_Usuario} = req.body
+router.put('/Articulo/:id', (req,res) => {
+  const {Nombre,Descripcion,Row_3} = req.body
   const { id } = req.params 
 
-mysqlConnection.query(`UPDATE Reciclaje SET Lugar_de_encuentro = ?,Fecha_y_hora = ?,Descripcion= ?,ID_Usuario= ? WHERE id = ?`,[Lugar_de_encuentro,Fecha_y_hora,Descripcion,ID_Usuario,id], (err, rows,fields) => {
+mysqlConnection.query(`UPDATE Articulo SET Nombre = ?,Descripcion = ?,Row_3= ? WHERE id = ?`,[Nombre,Descripcion,Row_3,id], (err, rows,fields) => {
    if(!err){
-    res.json({status: `El Reciclaje ha sido actualizado con éxito`});
+    res.json({status: `Articulo ha sido actualizado con éxito`});
    }else{
      console.log(err);
    }
